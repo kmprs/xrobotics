6e38608066ffd59149ac23eaab08720472a54b62
Jakob Kampers <jakobkampers23@gmail.com>
2026-07-01 06:56:22 +0200

    feat: prettify ui

--- ui/js/tree.js
+++ ui/js/tree.js
@@ -1,31 +1,20 @@
 function createNode(node) {
-
     const li = document.createElement("li");
     li.textContent = node.name;
-
     if (node.children && node.children.length > 0) {
-
         const ul = document.createElement("ul");
-
         node.children.forEach(child => {
             ul.appendChild(createNode(child));
         });
-
         li.appendChild(ul);
     }
-
     return li;
 }
 
 export function renderTree(containerId, tree) {
-
     const container = document.getElementById(containerId);
-
     container.innerHTML = "";
-
     const ul = document.createElement("ul");
-
     ul.appendChild(createNode(tree));
-
     container.appendChild(ul);
 }
